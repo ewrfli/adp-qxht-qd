fa778a9e01a5e9fb5846bc1b414a5cd53953e2f3
ewrfli <q304062@163.com>
2021-12-09 18:12:14 +0800

    FEAT add monitor search

--- src/pages/Monitor/index.jsx
+++ src/pages/Monitor/index.jsx
@@ -3,53 +3,41 @@ import { HeartTwoTone, SmileTwoTone } from '@ant-design/icons';
 import { Card, Typography, Alert } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { useIntl } from 'umi';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Input } from 'antd';
+import { history } from 'umi';
+
+
+const handleFormSubmit = (value) => {
+  // eslint-disable-next-line no-console
+  console.log(value);
+};
 
 const Monitor = () => {
   const intl = useIntl();
   return (
-    <Card></Card>
-    // <PageHeaderWrapper
-    //   content={intl.formatMessage({
-    //     id: 'pages.admin.subPage.title',
-    //     defaultMessage: 'This page can only be viewed by admin',
-    //   })}
-    // >
-    //   <Card>
-    //     <Alert
-    //       message={intl.formatMessage({
-    //         id: 'pages.welcome.alertMessage',
-    //         defaultMessage: 'Faster and stronger heavy-duty components have been released.',
-    //       })}
-    //       type="success"
-    //       showIcon
-    //       banner
-    //       style={{
-    //         margin: -12,
-    //         marginBottom: 48,
-    //       }}
-    //     />
-    //     <Typography.Title
-    //       level={2}
-    //       style={{
-    //         textAlign: 'center',
-    //       }}
-    //     >
-    //       <SmileTwoTone /> Ant Design Pro <HeartTwoTone twoToneColor="#eb2f96" /> You
-    //     </Typography.Title>
-    //   </Card>
-    //   <p
-    //     style={{
-    //       textAlign: 'center',
-    //       marginTop: 24,
-    //     }}
-    //   >
-    //     Want to add more pages? Please refer to{' '}
-    //     <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
-    //       use block
-    //     </a>
-    //     。
-    //   </p>
-    // </PageHeaderWrapper>
+    <PageContainer
+      content={
+        <div
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          <Input.Search
+            placeholder="请输入"
+            enterButton="搜索"
+            size="large"
+            onSearch={handleFormSubmit}
+            style={{
+              maxWidth: 522,
+              width: '100%',
+            }}
+          />
+        </div>
+      }
+    >
+      <Card></Card>
+    </PageContainer>
   );
 };
 
