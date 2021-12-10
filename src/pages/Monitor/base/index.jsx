67bc98596a4570052eb74b3a14816d875c09daa5
ewrfli <q304062@163.com>
2021-12-10 17:24:39 +0800

    feat: add base charts page

--- src/pages/Monitor/base/index.jsx
+++ src/pages/Monitor/base/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Line } from '@ant-design/charts';
 import { HeartTwoTone, SmileTwoTone } from '@ant-design/icons';
 import { Layout, Card, Typography, Alert, Tabs, Radio } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
@@ -7,27 +8,41 @@ import { useIntl } from 'umi';
 const { Header, Content, Footer, Sider } = Layout;
 const { TabPane } = Tabs;
 
-// const Base = () => {
-//   const intl = useIntl();
-//   return (
-//     <Card></Card>
-    
-   
-//   );
-// };
 
 class Base extends React.Component {
   state = { size: 'small' };
 
   render() {
     const { size } = this.state;
+    const data = [
+      { year: '1991', value: 3 },
+      { year: '1992', value: 4 },
+      { year: '1993', value: 3.5 },
+      { year: '1994', value: 5 },
+      { year: '1995', value: 4.9 },
+      { year: '1996', value: 6 },
+      { year: '1997', value: 7 },
+      { year: '1998', value: 9 },
+      { year: '1999', value: 13 },
+    ];
+    const config = {
+      data,
+      height: 400,
+      xField: 'year',
+      yField: 'value',
+      point: {
+        size: 5,
+        shape: 'diamond',
+      },
+    };
     return (
       <Layout> 
         <Content style={{ padding: '0 0' }}>
-          <Card style={{ paddingTop: '0px' }}>
+          {/* <Card style={{ paddingTop: '0px' }}> */}
             <Tabs defaultActiveKey="1" type="card" size={size}>
               <TabPane tab="Card Tab 1" key="1">
-                Content of card tab 1
+                <Card style={{ width: '500px' }}><Line {...config} /></Card>
+                <Card style={{ width: '500px' }}><Line {...config} /></Card>
               </TabPane>
               <TabPane tab="Card Tab 2" key="2">
                 Content of card tab 2
@@ -39,7 +54,7 @@ class Base extends React.Component {
                 Content of card tab 4
               </TabPane>
             </Tabs>
-          </Card>
+          {/* </Card> */}
         </Content>
       </Layout>
     );
